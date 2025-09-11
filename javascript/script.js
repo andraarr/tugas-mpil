3f5629f3348ad24cb5d0cfcb944135d7a7748830
andraarr <agniandra34@gmail.com>
2025-09-11 10:17:28 +0700

    membuat file script.js

---
+++ javascript/script.js
@@ -0,0 +1,7 @@
+let nama ="andra";
+let jumlahHuruf =nama.length;
+let hasil=jumlahHuruf *5;
+
+console.log("nama:",nama);
+console.log("jumlah huruf:",jumlahHuruf);
+console.log("hasil(jumlah huruf x 5):",);
